3f944bf15abceeba84a692edec52c86e9d854895
Dmitry Ryzhenkov <cuicui@arch>
2024-08-25 03:43:37 +0200

    refactor aun mejor

--- routes/progress.py
+++ routes/progress.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-from skills import Clicking
+from skills import Clicking, Tracking, Switching
 
 def run(anchor, db) -> None:
     username = anchor.text_input(label="Username", key="username_2")
@@ -17,69 +17,19 @@ def run(anchor, db) -> None:
         if len(df) == 0:
             anchor.warning("No hay registros")
         else:
-            skill = anchor.selectbox(label="Benchmark", options=["Clicking", "Tracking", "Switching"])
-            progress_dashboard(anchor=anchor, df=df, skill=skill)
+            progress_dashboard(anchor=anchor, df=df)
 
-def rank(score, thresholds):
-    ranks = ["Silver", "Gold", "Platinum", "Diamond", "Jade", "Master", "Grandmaster", "Nova", "Astra", "Celestial"]
-    progress = score/thresholds[-1]
-    for i, threshold in enumerate(thresholds):
-        if score >= threshold:
-            return score, ranks[i], progress
-    return score, "Bronze", progress
+def polar_plot(latest, best, skill) -> go.Figure:
+    latest = latest.to_dict()
+    best = best.to_dict()
+    progress_best = [x[2] for x in skill.get_ranks(scores=list(best.values()))]
+    progress_latest = [x[2] for x in skill.get_ranks(scores=list(latest.values()))]
 
-def progress_dashboard(anchor, df, skill) -> None:
-    # Ranking
-    skills = {
-        "tracking.smoothbot_easy" : "tracking.precise",
-        "tracking.air_angelic_4_easy" : "tracking.precise",
-        "tracking.pgti_easy" : "tracking.precise",
-        "tracking.fuglaaxyz_easy" : "tracking.reactive",
-        "tracking.ground_plaza_easy" : "tracking.reactive",
-        "tracking.air_easy" : "tracking.reactive",
-        "switching.patts_easy" : "switching.speed",
-        "switching.psalmts_easy" : "switching.speed",
-        "switching.voxts_easy" : "switching.speed",
-        "switching.kints_easy" : "switching.evasive",
-        "switching.b180t_easy" : "switching.evasive",
-        "switching.smoothbot_ts_easy" : "switching.evasive",
-    }
-
-    # Clicking
-    clicking = Clicking()
-    clicking_latest = df[[col for col in df.columns if "clicking" in col]].iloc[-1]
-    dyn_clicking = clicking.dynamic_rank(scores=clicking_latest.iloc[:3].to_list())
-    stat_clicking = clicking.static_rank(scores=clicking_latest.iloc[3:].to_list()) 
-
-    anchor.write(dyn_clicking)
-    anchor.write(stat_clicking)
-    
-    clicking_best = df[[col for col in df.columns if "clicking" in col]].max(axis=0).to_dict()
-    clicking_latest = clicking_latest.to_dict()
-    progress_best = [x[2] for x in clicking.get_ranks(scores=list(clicking_best.values()))]
-    progress_latest = [x[2] for x in clicking.get_ranks(scores=list(clicking_latest.values()))]
-
-    tracking_thresholds = [
-        [1700,2100,2400,2800,3100],
-        [1800,2200,2600,3000,3300],
-        [700,1000,1300,1600,1900],
-        [7500,8500,10000,12000,14000],
-        [835, 845, 855, 870, 880],
-        [775, 800, 825, 852, 865]
-    ]
-    switching_thresholds = [
-        [65,80,85,91,98],
-        [50,58,67,76,85],
-        [74,82,90,100,109],
-        [45,54,60,66,72],
-        [40, 51, 60, 69, 77],
-        [35, 42, 46, 50, 54]
-    ]
     fig = go.Figure()
 
     fig.add_trace(go.Scatterpolar(
         r=progress_best,
-        theta=list(clicking_best.keys()),
+        theta=[key.split(".")[1] for key in best.keys()],
         fill="toself",
         fillcolor="rgba(255,127,0,0.3)",
         line=dict(color="rgba(0,0,0,0)"),
@@ -89,7 +39,7 @@ def progress_dashboard(anchor, df, skill) -> None:
 
     fig.add_trace(go.Scatterpolar(
         r=progress_latest + [progress_latest[0]],
-        theta=list(clicking_latest.keys()) + [list(clicking_latest.keys())[0]],
+        theta=[key.split(".")[1] for key in latest.keys()] + [list(latest.keys())[0].split(".")[1]],
         fill="toself",
         fillcolor="rgba(0,0,0,0)",
         line=dict(color="rgba(0,0,255,0.7)"),
@@ -101,4 +51,39 @@ def progress_dashboard(anchor, df, skill) -> None:
         polar=dict(radialaxis=dict(range=[0,1]))
     )
 
-    anchor.plotly_chart(fig)
+    return fig
+
+def progress_dashboard(anchor, df) -> None:
+    # Skills
+    clicking = Clicking()
+    clicking_latest = df[[col for col in df.columns if "clicking" in col]].dropna().iloc[-1]
+    clicking_best = df[[col for col in df.columns if "clicking" in col]].max(axis=0)
+    dyn_clicking_rank = clicking.dynamic_rank(scores=clicking_latest.iloc[:3].to_list())
+    stat_clicking_rank = clicking.static_rank(scores=clicking_latest.iloc[3:].to_list()) 
+    clicking_fig = polar_plot(clicking_latest, clicking_best, clicking)
+
+    tracking = Tracking()
+    tracking_latest = df[[col for col in df.columns if "tracking" in col]].dropna().iloc[-1]
+    tracking_best = df[[col for col in df.columns if "tracking" in col]].max(axis=0)
+    prec_tracking_rank = tracking.precise_rank(scores=tracking_latest.iloc[:3].to_list())
+    reac_tracking_rank = tracking.reactive_rank(scores=tracking_latest.iloc[3:].to_list()) 
+    tracking_fig = polar_plot(tracking_latest, tracking_best, tracking)
+
+    switching = Switching()
+    switching_latest = df[[col for col in df.columns if "switching" in col]].dropna().iloc[-1]
+    switching_best = df[[col for col in df.columns if "switching" in col]].max(axis=0)
+    speed_switching_rank = switching.speed_rank(scores=switching_latest.iloc[:3].to_list())
+    eva_switching_rank = switching.evasive_rank(scores=switching_latest.iloc[3:].to_list()) 
+    switching_fig = polar_plot(switching_latest, switching_best, switching)
+
+    anchor.write(f"Dynamic clicking rank {dyn_clicking_rank}")
+    anchor.write(f"Static clicking rank {stat_clicking_rank}")
+    anchor.plotly_chart(clicking_fig)
+    
+    anchor.write(f"Precise tracking rank {prec_tracking_rank}")
+    anchor.write(f"Reactive tracking rank {reac_tracking_rank}")
+    anchor.plotly_chart(tracking_fig)
+
+    anchor.write(f"Speed switching rank {speed_switching_rank}")
+    anchor.write(f"Evasive switching rank {eva_switching_rank}")
+    anchor.plotly_chart(switching_fig)
--- skills.py
+++ skills.py
@@ -9,10 +9,10 @@ class Scenario:
 
     def get_rank(self, score):
         progress = score/self.thresholds[-1]
-        rank = RANKS[0]
+        rank = "Unranked"
         for i, threshold in enumerate(self.thresholds):
             if score >= threshold:
-                rank = RANKS[i+1]
+                rank = RANKS[i]
         return score, rank, progress
  
 class Clicking:
@@ -32,24 +32,114 @@ class Clicking:
 
     def dynamic_rank(self, scores) -> tuple[str, str]:
         ranks = [scen.get_rank(score)[1] for scen, score in zip(self.dynamic_scenarios, scores)]
-        best = "Bronze"
+        best = "Unrated"
         for rank in RANKS:
             if rank in ranks:
                 best = rank
         repeats = len([r for r in ranks if r == best])
         subrank = SUBRANKS[repeats-1]
+        if best == "Unrated":
+            subrank = ""
         return best, subrank
 
     def static_rank(self, scores) -> tuple[str, str]:
         ranks = [scen.get_rank(score)[1] for scen, score in zip(self.static_scenarios, scores)]
-        best = "Bronze"
+        best = "Unrated"
         for rank in RANKS:
             if rank in ranks:
                 best = rank
         repeats = len([r for r in ranks if r == best])
         subrank = SUBRANKS[repeats-1]
+        if best == "Unrated":
+            subrank = ""
         return best, subrank
 
     def get_ranks(self, scores) -> list:
         scenarios = self.dynamic_scenarios + self.static_scenarios
+        return [scenario.get_rank(score) for scenario, score in zip(scenarios, scores)]
+
+class Tracking:
+
+    def __init__(self) -> None:
+        self.precise_scenarios = [
+            Scenario("smoothbot_easy", [1700,2100,2400,2800,3100]),
+            Scenario("air_angelic_4_easy", [1800,2200,2600,3000,3300]),
+            Scenario("pgti_easy", [700,1000,1300,1600,1900])
+        ]
+        self.reactive_scenarios = [
+            Scenario("fuglaaxyz_easy", [7500,8500,10000,12000,14000]),
+            Scenario("ground_plaza_easy", [835, 845, 855, 870, 880]),
+            Scenario("air_easy", [775, 800, 825, 852, 865])
+        ]
+
+    def precise_rank(self, scores):
+        ranks = [scen.get_rank(score)[1] for scen, score in zip(self.precise_scenarios, scores)]
+        best = "Unrated"
+        for rank in RANKS:
+            if rank in ranks:
+                best = rank
+        repeats = len([r for r in ranks if r == best])
+        subrank = SUBRANKS[repeats-1]
+        if best == "Unrated":
+            subrank = ""
+        return best, subrank
+
+
+    def reactive_rank(self, scores):
+        ranks = [scen.get_rank(score)[1] for scen, score in zip(self.reactive_scenarios, scores)]
+        best = "Unrated"
+        for rank in RANKS:
+            if rank in ranks:
+                best = rank
+        repeats = len([r for r in ranks if r == best])
+        subrank = SUBRANKS[repeats-1]
+        if best == "Unrated":
+            subrank = ""
+        return best, subrank
+
+    def get_ranks(self, scores) -> list:
+        scenarios = self.precise_scenarios + self.reactive_scenarios
+        return [scenario.get_rank(score) for scenario, score in zip(scenarios, scores)]
+
+class Switching:
+
+    def __init__(self) -> None:
+        self.speed_scenarios = [
+            Scenario("patts_easy", [65,80,85,91,98]),
+            Scenario("psalmts_easy", [50,58,67,76,85]),
+            Scenario("voxts_easy", [74,82,90,100,109])
+        ]
+        self.evasive_scenarios = [
+            Scenario("kints_easy", [45,54,60,66,72]),
+            Scenario("b180t_easy", [40, 51, 60, 69, 77]),
+            Scenario("smoothbot_ts_easy", [35, 42, 46, 50, 54])
+        ]
+
+    def speed_rank(self, scores):
+        ranks = [scen.get_rank(score)[1] for scen, score in zip(self.speed_scenarios, scores)]
+        best = "Unrated"
+        for rank in RANKS:
+            if rank in ranks:
+                best = rank
+        repeats = len([r for r in ranks if r == best])
+        subrank = SUBRANKS[repeats-1]
+        if best == "Unrated":
+            subrank = ""
+        return best, subrank
+
+
+    def evasive_rank(self, scores):
+        ranks = [scen.get_rank(score)[1] for scen, score in zip(self.evasive_scenarios, scores)]
+        best = "Unrated"
+        for rank in RANKS:
+            if rank in ranks:
+                best = rank
+        repeats = len([r for r in ranks if r == best])
+        subrank = SUBRANKS[repeats-1]
+        if best == "Unrated":
+            subrank = ""
+        return best, subrank
+
+    def get_ranks(self, scores) -> list:
+        scenarios = self.speed_scenarios + self.evasive_scenarios
         return [scenario.get_rank(score) for scenario, score in zip(scenarios, scores)]
